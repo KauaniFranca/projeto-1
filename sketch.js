4e21e0d30be195d96f0bddd28ec24ead53ffeae1
Kauani <148967475+KauaniFranca@users.noreply.github.com>
2023-11-01 08:06:05 -0300

    adicionando arquivos html  js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(400, 400);background("#9C27B0")
+  }
+  
+  function draw() {
+    
+    stroke("rgb(12,11,11)")
+    fill("#E91E63")
+    
+    if(mouseIsPressed) {
+    rect(mouseX,mouseY,20,20);
+  }
+    }
+    
